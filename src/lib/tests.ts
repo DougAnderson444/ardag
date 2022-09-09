db62b4d137086149c8baad81a9ee830a50224541
Doug A <douganderson444@gmail.com>
2022-09-09 12:27:57 -0300

    redesign to use txid as entries in SWC

--- src/lib/tests.ts
+++ src/lib/tests.ts
@@ -31,7 +31,7 @@ async function mine() {
 }
 
 export async function tests(): boolean {
-	let post, wallet;
+	let post, wallet, rootCID;
 
 	if (dev) {
 		// Save this buffer as an Arweave Contract Transaction
@@ -49,7 +49,13 @@ export async function tests(): boolean {
 
 		// need to bind transactions.post to arweave.transactions as *this*
 		const doPost = arweave.transactions.post;
-		post = doPost.bind(arweave.transactions);
+		const p = doPost.bind(arweave.transactions);
+		post = async (tx) => {
+			const resp = await p(tx);
+			await mine();
+			console.log('Mined', tx.id);
+			return resp;
+		};
 	} else {
 		arweave = Arweave.init({});
 		// use Bunldr for production
@@ -66,61 +72,58 @@ export async function tests(): boolean {
 	let v1 = { number: '555-1234' };
 	let v2 = { number: '+1-555-555-1234' };
 
-	// TX1
-	let rootCID = await dag.tx.add(key, v1);
-	const savedBuffer = await dag.tx.commit();
-	const { cid: carCid } = await encode(savedBuffer);
+	async function aceTap(tag: string, obj: object) {
+		// Add, commit, encode, Tag and Post
+		// TX1
+		rootCID = await dag.tx.add(tag, obj);
+		const savedBuffer = await dag.tx.commit();
+		const { cid: carCid } = await encode(savedBuffer);
 
-	// TX2
-	let rootCID2 = await dag.tx.add(key, v2);
-	const savedBuffer2 = await dag.tx.commit();
-	const { cid: carCid2 } = await encode(savedBuffer2);
+		// create an Arweave data transaction
+		let tx = await arweave.createTransaction({ data: savedBuffer });
+
+		const tags = [
+			{ name: 'Root-CID', value: [rootCID.toString()] },
+			{ name: 'CAR-CID', value: [carCid.toString()] }
+		];
+
+		if (tags && tags.length) {
+			for (const tag of tags) {
+				tx.addTag(tag.name.toString(), tag.value.toString());
+			}
+		}
+		// post it to Arweave
+		await arweave.transactions.sign(tx, wallet.jwk);
+		await post(tx);
+		return tx.id;
+	}
+
+	let txId1 = await aceTap(key, v1);
+	let txId2 = await aceTap(key, v2);
 
 	/**
-	 * Now that you have 2 transactions, save them to Arweave via ArDagTxs
+	 * Now that you have 2 transactions, save their TxIds to SmArTweave contract via ArDagTxs
 	 */
 	const ardag = createArDag({ arweave, post });
 
 	// create a contract if you don't have one
 	const contractId = await ardag.createNewArDag(wallet.jwk); // uses wallet if no jwk specified
 
-	const tags = [
-		{ name: 'Root-CID', value: [rootCID.toString()] },
-		{ name: 'CAR-CID', value: [carCid.toString()] }
-	];
-	let input = {
-		function: 'ArDagTx',
-		ardagtx: encodeURLSafe(savedBuffer)
-	};
+	async function crisp(contractId: string, ardagtxid: string) {
+		// Create, Sign, Post
+		const tx = await ardag.createTx(contractId, { function: 'ArDagTx', ardagtxid });
+		await arweave.transactions.sign(tx, wallet.jwk);
+		console.log('Contract Updated with', ardagtxid);
+		await post(tx);
+	}
 
-	// write the 1st data buffer to this log
-	const transaction1 = await ardag.createTx(contractId, input, tags);
-	await arweave.transactions.post(transaction1);
-
-	const secondTags = [
-		{ name: 'Root-CID', value: [rootCID2.toString()] },
-		{ name: 'CAR-CID', value: [carCid2.toString()] }
-	];
-	const input2 = {
-		function: 'ArDagTx',
-		ardagtx: encodeURLSafe(savedBuffer2)
-	};
-
-	// write the 2nd data buffer to this log
-	const transaction2 = await ardag.createTx(contractId, input2, secondTags);
-	await arweave.transactions.post(transaction2);
-	// const txid2 = await ardag.post(transaction2);
+	await crisp(contractId, txId1);
+	await crisp(contractId, txId2);
 
 	// load buffer from Contract transactions
 	const searchTags = [
-		{
-			name: 'App-Name',
-			values: ['SmartWeaveAction'] // SmartWeaveContract
-		},
-		{
-			name: 'Contract',
-			values: [contractId]
-		}
+		{ name: 'App-Name', values: ['SmartWeaveAction'] },
+		{ name: 'Contract', values: [contractId] }
 	];
 
 	const txs = await ardb.search('transactions').tags(searchTags).findAll();
@@ -128,16 +131,18 @@ export async function tests(): boolean {
 	// now read the buffers from Arweave and load them into a new dag, see if they match
 	const rebuiltDag = await createDagRepo({ path: 'rebuiltDag' }); // make a barebones dag repo for fast loading
 
-	txs.forEach(async (tx) => {
-		console.log('tx', tx);
+	for (const tx of txs) {
 		const parsed = JSON.parse(tx.tags.find((el) => el.name === 'Input').value);
-		const buffer = new Uint8Array(decodeURLSafe(parsed.ardagtx));
-		await rebuiltDag.importBuffer(buffer); // as many as you need
-	});
+		const txid = parsed.ardagtxid;
+		const data = await arweave.transactions.getData(txid);
+		const buffer = new Uint8Array(decodeURLSafe(data));
+		const r = await rebuiltDag.importBuffer(buffer); // as many as you need
+		console.log(`Loaded from Arweave ${r.toString()}`);
+	}
 
-	const rebuiltCurrent = (await rebuiltDag.get(rootCID2, { path: `/${key}/current/number` })).value;
+	const rebuiltCurrent = (await rebuiltDag.get(rootCID, { path: `/${key}/current/number` })).value;
 	console.log(rebuiltCurrent, v2.number == rebuiltCurrent);
-	const rebuiltPrev = (await rebuiltDag.get(rootCID2, { path: `/${key}/prev/number` })).value;
+	const rebuiltPrev = (await rebuiltDag.get(rootCID, { path: `/${key}/prev/number` })).value;
 	console.log(rebuiltPrev, v1.number == rebuiltPrev);
 
 	return v1.number == rebuiltPrev && v2.number == rebuiltCurrent;
