a32abcca136404568ca19f6fa0b6f0e606162a3e
Doug A <douganderson444@gmail.com>
2022-09-21 10:11:49 -0300

    add resolver

---
+++ src/lib/components/Resolver.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let address = '';
+
+	onMount(async () => {
+		const { initializeArDag, load } = await import('@douganderson444/ardag');
+	});
+</script>
+
+<div class="">
+	<div class="">
+		<input type="text" bind:value={address} placeholder="Enter Arweave address" />
+		<button
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			on:click={() => {
+				console.log(address);
+			}}>Go</button
+		>
+	</div>
+</div>
+{#if address}{/if}
